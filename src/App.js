d5b12effdb3a00bceee4282951f2203d7aa1b5de
Pedro Cobos <pedro.cobos@live.com>
2021-01-27 14:39:25 -0600

    feat: add decrement action on App Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { increment } from './actions';
+import { increment, decrement } from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter)
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <h1>Counter {counter}</h1>
       <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
