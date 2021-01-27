043e8fee7c42bba55b56229a1290f3fe88640b17
Pedro Cobos <pedro.cobos@live.com>
2021-01-26 19:11:01 -0600

    feat: redux intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>React Redux</h1>
+      <h1>React Redux 101</h1>
     </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+// STORE => GLOBALIZED STATE
+
+// ACTION
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT': 
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+
+let store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(decrement());
+store.dispatch(increment());
+
+
 
 ReactDOM.render(
   <React.StrictMode>
