2098dae9c2d44bb88c161b686dd14dd574cd0367
Pedro Cobos <pedro.cobos@live.com>
2021-01-27 14:50:51 -0600

    feat: add payload to increment action

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
